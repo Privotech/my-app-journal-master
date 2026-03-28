cde98df8278e378d15d6f0a30e0b00532746c85e
Privotech <oyegbileprivilege@gmail.com>
2026-03-28 14:35:09 +0100

    solve route err

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -140,7 +140,7 @@ const Login = () => {
               <div className="text-center">
                 <p className="text-sm text-gray-600">
                   Don't have an account?{' '}
-                  <Link to="/home" className="font-medium text-blue-600 hover:text-blue-500 transition-colors">
+                  <Link to="/signup" className="font-medium text-blue-600 hover:text-blue-500 transition-colors">
                     Sign up
                   </Link>
                 </p>
